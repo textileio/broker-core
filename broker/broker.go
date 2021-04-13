943fee196af660eaa8db6946010e66805aa6d568
Ignacio Hagopian <jsign.uy@gmail.com>
2021-04-13 09:13:00 -0300

    add metadata as part of BrokerRequest
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- broker/broker.go
+++ broker/broker.go
@@ -6,24 +6,31 @@ import (
 	"github.com/ipfs/go-cid"
 )
 
+// Broker allows to create and track BrokerRequest.
 type Broker interface {
 	Create(ctx context.Context, c cid.Cid, meta Metadata) (BrokerRequest, error)
 }
 
+// BrokerRequest references a storage request for a Cid.
 type BrokerRequest struct {
-	ID     string              `json:"id"`
-	Status BrokerRequestStatus `json:"status"`
+	ID       string              `json:"id"`
+	Status   BrokerRequestStatus `json:"status"`
+	Metadata Metadata            `json:"metadata"`
 }
 
+// Metadata provides storage and bidding configuration.
 type Metadata struct {
 	Region string `json:"region"`
 }
 
+// BrokerRequestStatus describe the current status of a
+// BrokerRequest.
 type BrokerRequestStatus int
 
-// Not complete yet.
 const (
 	StatusUnkown BrokerRequestStatus = iota
+	StatusIdle
+	StatusFetchingData
 	StatusBatching
 	StatusPreparing
 	StatusBidding
