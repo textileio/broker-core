2e07436aa120626bcc87277746b6579c2f54b680
Ignacio Hagopian <jsign.uy@gmail.com>
2021-04-22 15:54:10 -0300

    packerd/packer/test: add extra test for overlapping cids & dockerize ipfs dependency
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/packerd/packer/packer_test.go
+++ cmd/packerd/packer/packer_test.go
@@ -15,16 +15,20 @@ import (
 	"github.com/ipfs/interface-go-ipfs-core/options"
 	"github.com/ipfs/interface-go-ipfs-core/path"
 	"github.com/multiformats/go-multiaddr"
+	"github.com/ory/dockertest/v3"
 	"github.com/stretchr/testify/require"
 	"github.com/textileio/broker-core/broker"
 	"github.com/textileio/broker-core/tests"
 )
 
-func TestPackAndRetrieve(t *testing.T) {
+func TestPack(t *testing.T) {
+	t.Parallel()
 	ctx := context.Background()
 
+	ipfsDocker := launchIPFSContainer(t)
+	ipfsAPIMultiaddr := "/ip4/127.0.0.1/tcp/" + ipfsDocker.GetPort("5001/tcp")
+
 	// 1- Create a Packer and have a go-ipfs client too.
-	ipfsAPIMultiaddr := "/ip4/127.0.0.1/tcp/5001"
 	ma, err := multiaddr.NewMultiaddr(ipfsAPIMultiaddr)
 	require.NoError(t, err)
 	ipfs, err := httpapi.NewApi(ma)
@@ -34,7 +38,8 @@ func TestPackAndRetrieve(t *testing.T) {
 	packer := createPacker(t, ipfs, brokerMock)
 
 	// 2- Add 100 random files and get their cids.
-	dataCids := addRandomData(t, ipfs, 100)
+	numFiles := 100
+	dataCids := addRandomData(t, ipfs, numFiles)
 
 	// 3- Signal ready to pack these cids to Packer
 	for i, dataCid := range dataCids {
@@ -43,27 +48,70 @@ func TestPackAndRetrieve(t *testing.T) {
 	}
 
 	// 4- Force pack and inspect what was signaled to the broker
-	err = packer.pack(ctx)
+	numBatchedCids, err := packer.pack(ctx)
 	require.NoError(t, err)
 
-	require.Len(t, brokerMock.srids, 100)
+	require.Len(t, brokerMock.srids, numFiles)
+	require.Equal(t, int64(numFiles), numBatchedCids)
 	require.True(t, brokerMock.batchCid.Defined())
 
+	// Check that the batch cid was pinned in ipfs.
 	_, pinned, err := ipfs.Pin().IsPinned(ctx, path.IpfsPath(brokerMock.batchCid))
 	require.NoError(t, err)
 	require.True(t, pinned)
+}
 
-	// TODO:
-	// - Add test to check that three levels are < 1MiB in worst case scenario.
+func TestMultipleBrokerRequestWithSameCid(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	ipfsDocker := launchIPFSContainer(t)
+	ipfsAPIMultiaddr := "/ip4/127.0.0.1/tcp/" + ipfsDocker.GetPort("5001/tcp")
+
+	ma, err := multiaddr.NewMultiaddr(ipfsAPIMultiaddr)
+	require.NoError(t, err)
+	ipfs, err := httpapi.NewApi(ma)
+	require.NoError(t, err)
+
+	brokerMock := &brokerMock{}
+	packer := createPacker(t, ipfs, brokerMock)
+
+	// 2- Create a single file
+	dataCid := addRandomData(t, ipfs, 1)[0]
+
+	// 3- Simulate multiple BrokerRequests but with the same data
+	numRepeatedBrokerRequest := 100
+	for i := 0; i < numRepeatedBrokerRequest; i++ {
+		err = packer.ReadyToPack(ctx, broker.BrokerRequestID(strconv.Itoa(i)), dataCid)
+		require.NoError(t, err)
+	}
+
+	// 4- Force pack and inspect what was signaled to the broker
+	numCidsBatched, err := packer.pack(ctx)
+	require.NoError(t, err)
+
+	require.True(t, brokerMock.batchCid.Defined())
+	// We fullfiled numRepeatedBrokerRequest, not only 1!
+	require.Len(t, brokerMock.srids, numRepeatedBrokerRequest)
+	// Despite we fulfilled multiple broker request, the batch only has one cid!
+	require.Equal(t, int64(1), numCidsBatched)
+
+	// Check that the batch cid was pinned in ipfs.
+	_, pinned, err := ipfs.Pin().IsPinned(ctx, path.IpfsPath(brokerMock.batchCid))
+	require.NoError(t, err)
+	require.True(t, pinned)
+}
+
+func TestSizeLimit(t *testing.T) {
+	panic("TODO")
 }
 
 func TestSelectorRetrieval(t *testing.T) {
-	// TODO
+	t.Skipf("Not implemented yet, needs to wait a bit for some things to land in ipfs world")
 }
 
 func createPacker(t *testing.T, ipfsClient *httpapi.HttpApi, broker *brokerMock) *Packer {
 	ds := tests.NewTxMapDatastore()
-	// TODO: dockerize?
 	packer, err := New(ds, ipfsClient, broker, WithFrequency(time.Hour))
 	require.NoError(t, err)
 
@@ -76,7 +124,7 @@ func addRandomData(t *testing.T, ipfs *httpapi.HttpApi, count int) []cid.Cid {
 
 	cids := make([]cid.Cid, count)
 	for i := 0; i < count; i++ {
-		data := make([]byte, 1000)
+		data := make([]byte, 100)
 		_, _ = r.Read(data)
 
 		node, err := ipfs.Unixfs().Add(
@@ -124,4 +172,23 @@ func (bm *brokerMock) Create(ctx context.Context, c cid.Cid, meta broker.Metadat
 // Get returns a broker request from an id.
 func (bm *brokerMock) Get(ctx context.Context, ID broker.BrokerRequestID) (broker.BrokerRequest, error) {
 	panic("shouldn't be called")
+}
+
+func launchIPFSContainer(t *testing.T) *dockertest.Resource {
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err)
+
+	ipfsDocker, err := pool.Run("ipfs/go-ipfs", "v0.8.0", []string{"IPFS_PROFILE=test"})
+	require.NoError(t, err)
+
+	err = ipfsDocker.Expire(180)
+	require.NoError(t, err)
+
+	time.Sleep(time.Second * 3)
+	t.Cleanup(func() {
+		err = pool.Purge(ipfsDocker)
+		require.NoError(t, err)
+	})
+
+	return ipfsDocker
 }
