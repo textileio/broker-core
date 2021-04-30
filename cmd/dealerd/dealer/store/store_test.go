ae832b828892eff1acbddaba177b4de68771a1b8
Ignacio Hagopian <jsign.uy@gmail.com>
2021-04-30 11:33:08 -0300

    dealer/store/tests: add tests for almost all apis
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/dealerd/dealer/store/store_test.go
+++ cmd/dealerd/dealer/store/store_test.go
@@ -88,28 +88,114 @@ func TestCreateFail(t *testing.T) {
 
 func TestRehydrate(t *testing.T) {
 	t.Parallel()
-	t.Skip()
+	ds := tests.NewTxMapDatastore()
+	s, err := New(ds)
+	require.NoError(t, err)
+
+	err = s.Create(gad1, []AuctionDeal{gaud1})
+	require.NoError(t, err)
+
+	// Recreate store with same ds.
+	s, err = New(ds)
+	require.NoError(t, err)
+	deepCheckAuctionData(t, s, gad1)
+	deepCheckAuctionDeals(t, s, gaud1)
 }
 
 func TestSaveAuctionDeal(t *testing.T) {
 	t.Parallel()
+
+	s, err := New(tests.NewTxMapDatastore())
+	require.NoError(t, err)
+
+	err = s.Create(gad1, []AuctionDeal{gaud1})
+	require.NoError(t, err)
+
+	auds, err := s.GetAllAuctionDeals(Pending)
+	require.NoError(t, err)
+
+	auds[0].Status = WaitingConfirmation
+	auds[0].ErrorCause = "duke"
+	auds[0].ProposalCid = castCid("QmdKDf5nepPLXErXd1pYY8hA82yjMaW3fdkU8D8kiz3jZ1")
+	auds[0].DealID = 1234
+	auds[0].DealExpiration = 5678
+	err = s.SaveAuctionDeal(auds[0])
+	require.NoError(t, err)
+	deepCheckAuctionDeals(t, s, auds[0])
 }
 
 func TestSaveAuctionDealFail(t *testing.T) {
 	t.Parallel()
+
 	t.Run("not found", func(t *testing.T) {
-		t.Skip()
+		t.Parallel()
+
+		s, err := New(tests.NewTxMapDatastore())
+		require.NoError(t, err)
+		aud := gaud1
+		aud.ID = "invented"
+		err = s.SaveAuctionDeal(aud)
+		require.Equal(t, ErrNotFound, err)
+	})
+	t.Run("wrong status transition", func(t *testing.T) {
+		t.Parallel()
+
+		s, err := New(tests.NewTxMapDatastore())
+		require.NoError(t, err)
+		err = s.Create(gad1, []AuctionDeal{gaud1})
+		require.NoError(t, err)
+		auds, err := s.GetAllAuctionDeals(Pending)
+		require.NoError(t, err)
+
+		auds[0].Status = Success
+		err = s.SaveAuctionDeal(auds[0])
+		require.Error(t, err)
 	})
 }
 
 func TestGetAllAuctionDeals(t *testing.T) {
 	t.Parallel()
-	t.Skip()
+
+	s, err := New(tests.NewTxMapDatastore())
+	require.NoError(t, err)
+
+	err = s.Create(gad1, []AuctionDeal{gaud1, gaud2})
+	require.NoError(t, err)
+	deepCheckAuctionData(t, s, gad1)
+	deepCheckAuctionDeals(t, s, gaud1, gaud2)
+
+	auds, err := s.GetAllAuctionDeals(Pending)
+	require.NoError(t, err)
+	require.Len(t, auds, 2)
+	cmpAuctionDeals(t, gaud1, auds[0])
+	cmpAuctionDeals(t, gaud2, auds[1])
 }
 
 func TestGetAuctionData(t *testing.T) {
 	t.Parallel()
-	t.Skip()
+
+	s, err := New(tests.NewTxMapDatastore())
+	require.NoError(t, err)
+	err = s.Create(gad1, []AuctionDeal{gaud1})
+	require.NoError(t, err)
+	auds, err := s.GetAllAuctionDeals(Pending)
+	require.NoError(t, err)
+
+	ad, err := s.GetAuctionData(auds[0].AuctionDataID)
+	require.NoError(t, err)
+	cmpAuctionData(t, gad1, ad)
+}
+
+func TestGetAuctionNotFound(t *testing.T) {
+	t.Parallel()
+
+	s, err := New(tests.NewTxMapDatastore())
+	require.NoError(t, err)
+	err = s.Create(gad1, []AuctionDeal{gaud1})
+	require.NoError(t, err)
+
+	_, err = s.GetAuctionData("invented")
+	require.Equal(t, ErrNotFound, err)
 }
 
 func TestRemoveAuctionDeals(t *testing.T) {
@@ -128,14 +214,6 @@ func castCid(cidStr string) cid.Cid {
 func deepCheckAuctionData(t *testing.T, s *Store, ad AuctionData) {
 	t.Helper()
 
-	cmp := func(t *testing.T, ad1, ad2 AuctionData) {
-		require.Equal(t, ad1.StorageDealID, ad2.StorageDealID)
-		require.Equal(t, ad1.PayloadCid, ad2.PayloadCid)
-		require.Equal(t, ad1.PieceCid, ad2.PieceCid)
-		require.Equal(t, ad1.Duration, ad2.Duration)
-		require.Equal(t, ad1.PieceSize, ad2.PieceSize)
-	}
-
 	// Check value in cache.
 	var cacheAd *AuctionData
 	for _, v := range s.auctionData {
@@ -147,7 +225,7 @@ func deepCheckAuctionData(t *testing.T, s *Store, ad AuctionData) {
 	require.NotNil(t, cacheAd)
 	require.NotEmpty(t, cacheAd.ID)
 	require.NotEmpty(t, cacheAd.CreatedAt)
-	cmp(t, ad, *cacheAd)
+	cmpAuctionData(t, ad, *cacheAd)
 
 	// Check value in datastore.
 	buf, err := s.ds.Get(makeAuctionDataKey(cacheAd.ID))
@@ -156,19 +234,19 @@ func deepCheckAuctionData(t *testing.T, s *Store, ad AuctionData) {
 	d := gob.NewDecoder(bytes.NewReader(buf))
 	err = d.Decode(&dsAd)
 	require.NoError(t, err)
-	cmp(t, ad, *cacheAd)
+	cmpAuctionData(t, ad, *cacheAd)
+}
+
+func cmpAuctionData(t *testing.T, ad1, ad2 AuctionData) {
+	require.Equal(t, ad1.StorageDealID, ad2.StorageDealID)
+	require.Equal(t, ad1.PayloadCid, ad2.PayloadCid)
+	require.Equal(t, ad1.PieceCid, ad2.PieceCid)
+	require.Equal(t, ad1.Duration, ad2.Duration)
+	require.Equal(t, ad1.PieceSize, ad2.PieceSize)
 }
 
 func deepCheckAuctionDeals(t *testing.T, s *Store, auds ...AuctionDeal) {
 	t.Helper()
-
-	cmp := func(t *testing.T, aud1, aud2 AuctionDeal) {
-		require.Equal(t, aud1.Miner, aud2.Miner)
-		require.Equal(t, aud1.PricePerGiBPerEpoch, aud2.PricePerGiBPerEpoch)
-		require.Equal(t, aud1.StartEpoch, aud2.StartEpoch)
-		require.Equal(t, aud1.Verified, aud2.Verified)
-		require.Equal(t, aud1.FastRetrieval, aud2.FastRetrieval)
-	}
 
 	for _, aud := range auds {
 		// Check value in cache.
@@ -182,7 +260,7 @@ func deepCheckAuctionDeals(t *testing.T, s *Store, auds ...AuctionDeal) {
 		require.NotNil(t, cacheAud)
 		require.NotEmpty(t, cacheAud.ID)
 		require.NotEmpty(t, cacheAud.CreatedAt)
-		cmp(t, aud, *cacheAud)
+		cmpAuctionDeals(t, aud, *cacheAud)
 
 		// Check value in datastore.
 		buf, err := s.ds.Get(makeAuctionDealKey(cacheAud.ID))
@@ -193,8 +271,22 @@ func deepCheckAuctionDeals(t *testing.T, s *Store, auds ...AuctionDeal) {
 		require.NoError(t, err)
 		require.NotEmpty(t, cacheAud.ID)
 		require.NotEmpty(t, cacheAud.CreatedAt)
-		cmp(t, aud, dsAud)
+		require.NotEmpty(t, cacheAud.AuctionDataID)
+		cmpAuctionDeals(t, aud, dsAud)
 	}
+}
+
+func cmpAuctionDeals(t *testing.T, aud1, aud2 AuctionDeal) {
+	require.Equal(t, aud1.Miner, aud2.Miner)
+	require.Equal(t, aud1.PricePerGiBPerEpoch, aud2.PricePerGiBPerEpoch)
+	require.Equal(t, aud1.StartEpoch, aud2.StartEpoch)
+	require.Equal(t, aud1.Verified, aud2.Verified)
+	require.Equal(t, aud1.FastRetrieval, aud2.FastRetrieval)
+	require.Equal(t, aud1.Status, aud2.Status)
+	require.Equal(t, aud1.ErrorCause, aud2.ErrorCause)
+	require.Equal(t, aud1.ProposalCid, aud2.ProposalCid)
+	require.Equal(t, aud1.DealID, aud2.DealID)
+	require.Equal(t, aud1.DealExpiration, aud2.DealExpiration)
 }
 
 var (
@@ -210,6 +302,13 @@ var (
 		Miner:               "f011001",
 		PricePerGiBPerEpoch: 10,
 		StartEpoch:          20,
+		Verified:            true,
+		FastRetrieval:       true,
+	}
+	gaud2 = AuctionDeal{
+		Miner:               "f011002",
+		PricePerGiBPerEpoch: 11,
+		StartEpoch:          21,
 		Verified:            true,
 		FastRetrieval:       true,
 	}
