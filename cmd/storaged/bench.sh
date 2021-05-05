775a291138430af9309aea41ea30efea99341f9f
Aaron Steele <eightysteele@gmail.com>
2021-05-05 16:56:45 -0700

    update bench with near call

--- cmd/storaged/bench.sh
+++ cmd/storaged/bench.sh
@@ -11,6 +11,8 @@ SIZE=$2
 TOKEN="eyJhbGciOiJFZERTQVNoYTI1NiIsInR5cCI6IkpXVCIsImp3ayI6eyJrdHkiOiJPS1AiLCJjcnYiOiJFZDI1NTE5IiwieCI6IjZURnVRRzFGTHZ4UGxPdGFVbllFQlRlU3haa09GZ3VSSGZwNlN1Q1ZDbG89IiwidXNlIjoic2lnIn19.eyJhdWQiOiJhYXJvbmJyb2tlciIsImlzcyI6ImNhcnNvbmZhcm1lci50ZXN0bmV0Iiwic3ViIjoiZGlkOmtleTp6Nk1rdjlZa25rMzZlUzhwY1pkZjgyWXhIcnBpWmJZZDFFYlNld0R2WEM3amhRRDciLCJuYmYiOjE2MjAwODY2NDMsImlhdCI6MTYyMDA4NjY0MywiZXhwIjozNjAwMDAwMDE2MjAwODY2NjB9.XcGW8z7HEVy6gZl2ZP0yGPyetlcXal8d86_YKvIor8vFQWYS9zSu4vxYmKutmsVkVu2gsopkdF3hsw0_qjCLDQ=="
 
 echo $TARGET
+echo "Locking funds..."
+near call lock-box.testnet lockFunds '{ "brokerId": "lock-box.testnet", "accountId": "lock-box.testnet" }' --account-id "lock-box.testnet" --amount 1 --seedPhrase "$SEED"
 echo "Generating random file..."
 TMPFILE=$(mktemp)
 head -c ${SIZE} < /dev/urandom > $TMPFILE
