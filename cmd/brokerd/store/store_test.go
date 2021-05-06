ff0c0bbe502eb30c5ee3037f17d2d6387f2cbd5b
Ignacio Hagopian <jsign.uy@gmail.com>
2021-05-06 10:49:51 -0300

    brokerd: add test for storage deal creation linked with broker requests
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/brokerd/store/store_test.go
+++ cmd/brokerd/store/store_test.go
@@ -12,6 +12,8 @@ import (
 )
 
 func TestSaveAndGetBrokerRequest(t *testing.T) {
+	t.Parallel()
+
 	ctx := context.Background()
 	s := newStore(t)
 
@@ -59,11 +61,82 @@ func TestSaveAndGetBrokerRequest(t *testing.T) {
 	require.Equal(t, now.Unix(), br3.CreatedAt.Unix())
 }
 
+func TestCreateStorageDeal(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	s := newStore(t)
+
+	// 0- Test retrieving a non-existant storage deal
+	_, err := s.GetStorageDeal(ctx, broker.StorageDealID("fake"))
+	require.Error(t, ErrNotFound, err)
+
+	// 1- Create two broker requests.
+	now := time.Now()
+	br1 := broker.BrokerRequest{
+		ID:      "BR1",
+		DataCid: castCid("QmdKDf5nepPLXErXd1pYY8hA82yjMaW3fdkU8D8kiz3jH2"),
+		Status:  broker.RequestBatching,
+		Metadata: broker.Metadata{
+			Region: "asia",
+		},
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+	br2 := broker.BrokerRequest{
+		ID:      "BR1",
+		DataCid: castCid("QmdKDf5nepPLXErXd1pYY8hA82yjMaW3fdkU8D8kiz3jH1"),
+		Status:  broker.RequestBatching,
+		Metadata: broker.Metadata{
+			Region: "asia",
+		},
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+	err = s.SaveBrokerRequest(ctx, br1)
+	require.NoError(t, err)
+	err = s.SaveBrokerRequest(ctx, br2)
+	require.NoError(t, err)
+
+	// 2- Create a storage deal linked with those two broker request.
+	now = time.Now()
+	sd := broker.StorageDeal{
+		PayloadCid:       castCid("QmdKDf5nepPLXErXd1pYY8hA82yjMaW3fdkU8D8kiz3jB1"),
+		Status:           broker.StorageDealPreparing,
+		BrokerRequestIDs: []broker.BrokerRequestID{br1.ID, br2.ID},
+		CreatedAt:        now,
+		UpdatedAt:        now,
+	}
+	err = s.CreateStorageDeal(ctx, &sd)
+	require.NoError(t, err)
+
+	// 3- Get the created storage deal by id, and check that fields are coherent.
+	sd2, err := s.GetStorageDeal(ctx, sd.ID)
+	require.NoError(t, err)
+	require.Equal(t, sd.ID, sd2.ID)
+	require.Equal(t, sd.Status, sd2.Status)
+	require.Equal(t, len(sd.BrokerRequestIDs), len(sd2.BrokerRequestIDs))
+	require.Equal(t, sd.CreatedAt.Unix(), sd2.CreatedAt.Unix())
+	require.Equal(t, sd.UpdatedAt.Unix(), sd2.UpdatedAt.Unix())
+
+	// 4- Check that the underlying storage request, moved to Preparing and are linked to the created
+	//    storage deal!
+	gbr1, err := s.GetBrokerRequest(ctx, br1.ID)
+	require.NoError(t, err)
+	require.Equal(t, broker.RequestPreparing, gbr1.Status)
+	require.Equal(t, sd.ID, gbr1.StorageDealID)
+	require.True(t, time.Since(gbr1.UpdatedAt) < time.Second)
+	gbr2, err := s.GetBrokerRequest(ctx, br2.ID)
+	require.NoError(t, err)
+	require.Equal(t, broker.RequestPreparing, gbr2.Status)
+	require.Equal(t, sd.ID, gbr2.StorageDealID)
+	require.True(t, time.Since(gbr2.UpdatedAt) < time.Second)
+}
+
 func newStore(t *testing.T) *Store {
 	ds := tests.NewTxMapDatastore()
 	s, err := New(ds)
 	require.NoError(t, err)
-
 	return s
 }
 
