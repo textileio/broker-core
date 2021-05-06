ce24ec02df8c5b992eb7dc9d7c77b5e2b43fea8a
Aaron Steele <eightysteele@gmail.com>
2021-05-06 09:59:41 -0700

    make seedPhrase a required param

--- cmd/storaged/bench.sh
+++ cmd/storaged/bench.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ "$#" -ne 2 ]; then
-	echo "use $0 <target-url> <size>"
+if [ "$#" -ne 3 ]; then
+	echo "use $0 <target-url> <size> <seedPhrase>"
 	exit -1
 fi
 
 TARGET="${1}/upload"
 SIZE=$2
 TOKEN="eyJhbGciOiJFZERTQVNoYTI1NiIsInR5cCI6IkpXVCIsImp3ayI6eyJrdHkiOiJPS1AiLCJjcnYiOiJFZDI1NTE5IiwieCI6IjZURnVRRzFGTHZ4UGxPdGFVbllFQlRlU3haa09GZ3VSSGZwNlN1Q1ZDbG89IiwidXNlIjoic2lnIn19.eyJhdWQiOiJhYXJvbmJyb2tlciIsImlzcyI6ImNhcnNvbmZhcm1lci50ZXN0bmV0Iiwic3ViIjoiZGlkOmtleTp6Nk1rdjlZa25rMzZlUzhwY1pkZjgyWXhIcnBpWmJZZDFFYlNld0R2WEM3amhRRDciLCJuYmYiOjE2MjAwODY2NDMsImlhdCI6MTYyMDA4NjY0MywiZXhwIjozNjAwMDAwMDE2MjAwODY2NjB9.XcGW8z7HEVy6gZl2ZP0yGPyetlcXal8d86_YKvIor8vFQWYS9zSu4vxYmKutmsVkVu2gsopkdF3hsw0_qjCLDQ=="
+SEED=$3
 
 echo $TARGET
 echo "Locking funds..."
