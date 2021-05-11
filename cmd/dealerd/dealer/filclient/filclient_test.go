eb3e7db4eaac72f667af099ac4caee6dfa5b02d1
Ignacio Hagopian <jsign.uy@gmail.com>
2021-05-11 10:27:19 -0300

    dealerd/fileclinet/tests: add more skipped tests useful for manual runs
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/dealerd/dealer/filclient/filclient_test.go
+++ cmd/dealerd/dealer/filclient/filclient_test.go
@@ -2,23 +2,92 @@ package filclient
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"testing"
 	"time"
 
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/lotus/api/apistruct"
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/require"
+	"github.com/textileio/broker-core/cmd/dealerd/dealer/store"
+	"github.com/textileio/broker-core/logging"
 )
 
 func TestExecuteAuctionDeal(t *testing.T) {
 	t.Parallel()
-	t.Skip()
+	t.SkipNow()
+
+	client := create(t)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	pieceCid, err := cid.Decode("baga6ea4seaqmj45fgnl36bep72gnf4ib5degrslzuq6zyk2hjbhakt6cas464pi")
+	require.NoError(t, err)
+	payloadCid, err := cid.Decode("uAXASIFxC4XOlV43b01pJO6ptOSxf8E_JjXhQXdgW-oMQxkUF")
+	require.NoError(t, err)
+	ad := store.AuctionData{
+		PayloadCid: payloadCid,
+		PieceCid:   pieceCid,
+		PieceSize:  34359738368,
+		Duration:   525600,
+	}
+	aud := store.AuctionDeal{
+		Miner:               "f01278",
+		PricePerGiBPerEpoch: 0,
+		StartEpoch:          754395,
+		Verified:            false,
+		FastRetrieval:       true,
+	}
+	propCid, err := client.ExecuteAuctionDeal(ctx, ad, aud)
+	require.NoError(t, err)
+	fmt.Printf("propCid: %s", propCid)
+}
+
+func TestPublishedMessageAndDealOnChain(t *testing.T) {
+	t.Parallel()
+	t.SkipNow()
+
+	client := create(t)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	publishedMessage, err := cid.Decode("bafy2bzacec6yztd2zzhgef77tqtx6qv5nldzziylq7iizpmgx4577boclftig")
+	require.NoError(t, err)
+	proposalCid, err := cid.Decode("bafyreibru2chqj7wanixo6m5qnmamovvgby7672ws3yojzyttimu7fl72q")
+	require.NoError(t, err)
+
+	dealID, err := client.ResolveDealIDFromMessage(ctx, proposalCid, publishedMessage)
+	require.NoError(t, err)
+	require.Equal(t, int64(1919949), dealID)
+
+	active, expiration, err := client.CheckChainDeal(ctx, dealID)
+	require.NoError(t, err)
+	require.True(t, active)
+	require.Equal(t, uint64(1279995), expiration)
+}
+
+func TestCheckStatusWithMiner(t *testing.T) {
+	t.Parallel()
+	t.SkipNow()
+
+	client := create(t)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	proposalCid, err := cid.Decode("bafyreibru2chqj7wanixo6m5qnmamovvgby7672ws3yojzyttimu7fl72q")
+	require.NoError(t, err)
+	status, err := client.CheckDealStatusWithMiner(ctx, "f01278", proposalCid)
+	require.NoError(t, err)
+	fmt.Printf("%s\n", logging.MustJSONIndent(status))
+	fmt.Printf("%s\n", storagemarket.DealStatesDescriptions[status.State])
 }
 
 func TestGetChainHeight(t *testing.T) {
 	t.Parallel()
+
 	client := create(t)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
@@ -26,37 +95,6 @@ func TestGetChainHeight(t *testing.T) {
 	height, err := client.GetChainHeight(ctx)
 	require.NoError(t, err)
 	require.Greater(t, height, uint64(0))
-}
-
-func TestResolveDealIDFromMessage(t *testing.T) {
-	t.Parallel()
-	t.SkipNow()
-
-	client := create(t)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-
-	messageCid, err := cid.Decode("bafy2bzaceblpg3v7ay45wf2lrvhwnjv2cldticixzekh5wft6mklyzg6vwjbw")
-	require.NoError(t, err)
-	proposalCid, err := cid.Decode("bafyreiewepxw6o2cka5jovcvpjh72a2gdhhvpt5xsiov7g4ejiyaawnti4")
-	require.NoError(t, err)
-	dealID, err := client.ResolveDealIDFromMessage(ctx, proposalCid, messageCid)
-	require.NoError(t, err)
-	require.Equal(t, int64(1856658), dealID)
-}
-
-func TestCheckDealStatusWithMiner(t *testing.T) {
-	t.Parallel()
-	t.SkipNow()
-
-	client := create(t)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
-	defer cancel()
-
-	proposalCid, err := cid.Decode("bafyreig54fltdzbl6f7yyvhyu6gyayheawgpeqsyczmdh4obeaqszrdrpm")
-	require.NoError(t, err)
-	_, err = client.CheckDealStatusWithMiner(ctx, "f022352", proposalCid)
-	require.NoError(t, err)
 }
 
 func create(t *testing.T) *FilClient {
