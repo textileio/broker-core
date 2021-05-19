fc0fbaf6d0fff11bd3c6fc521a9f58899285f234
Ignacio Hagopian <jsign.uy@gmail.com>
2021-05-19 14:27:46 -0300

    dealerd: use metrics base package
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/dealerd/metrics/metrics.go
+++ cmd/dealerd/metrics/metrics.go
@@ -1,29 +1,11 @@
 package metrics
 
 import (
-	"context"
-
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/global"
 )
 
-var (
-	// AttrOK is a metric tag to indicate a successful operation.
-	AttrOK = attribute.Key("status").String("ok")
-	// AttrError is a metric tag to indicate a failed operation.
-	AttrError = attribute.Key("status").String("error")
-	// Meter is the global meter used for metrics.
-	Meter = metric.Must(global.Meter("dealerd"))
-)
+const Prefix = "dealerd"
 
-// MetricIncrCounter increments the specified Int64Counter by 1. Depending if err
-// is nil or not, it will use AttrOK or AttrError respectively. This method is a helper
-// for deferring in methods.
-func MetricIncrCounter(ctx context.Context, err error, m metric.Int64Counter) {
-	attr := AttrOK
-	if err != nil {
-		attr = AttrError
-	}
-	m.Add(ctx, 1, attr)
-}
+// Meter is the global meter used for metrics.
+var Meter = metric.Must(global.Meter(Prefix))
