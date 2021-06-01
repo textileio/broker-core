e21fd70de05cafa6149e38cfc54b15fbaf852739
Ignacio Hagopian <jsign.uy@gmail.com>
2021-06-01 08:57:33 -0300

    k8/ipfs-cluster: fix env var & add printing resolved config in logs [shipit]
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/dealerd/main.go
+++ cmd/dealerd/main.go
@@ -48,7 +48,7 @@ var rootCmd = &cobra.Command{
 		common.CheckErrf("setting log levels: %v", err)
 	},
 	Run: func(c *cobra.Command, args []string) {
-		settings, err := json.MarshalIndent(v.AllSettings(), "", "  ")
+		settings, err := marshalConfig(v)
 		common.CheckErr(err)
 		log.Infof("loaded config: %s", string(settings))
 
@@ -81,4 +81,12 @@ var rootCmd = &cobra.Command{
 
 func main() {
 	common.CheckErr(rootCmd.Execute())
+}
+
+func marshalConfig(v *viper.Viper) ([]byte, error) {
+	all := v.AllSettings()
+	if all["lotus-exported-wallet-address"].(string) != "" {
+		all["lotus-exported-wallet-address"] = "***"
+	}
+	return json.MarshalIndent(all, "", "  ")
 }
