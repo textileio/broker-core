a26d8963e397270faa55e06e044def01b5d838f1
Ignacio Hagopian <jsign.uy@gmail.com>
2021-07-05 17:47:22 -0300

    msgbroker: create message-broker abstraction layer
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ msgbroker/msgbroker.go
@@ -0,0 +1,15 @@
+package msgbroker
+
+// MessageID is the type of a message identifier.
+type MessageID string
+
+// AckMessageFunc is a function that ACK a received message.
+type AckMessageFunc func()
+
+// NackMessageFunc is a function that NACKs a received message.
+type NackMessageFunc func()
+
+// TopicHandler is function that processes a received message. The function
+// is responsible for calling Ack() or Nack() functions to properly signal
+// message processing.
+type TopicHandler func(MessageID, []byte, AckMessageFunc, NackMessageFunc)
