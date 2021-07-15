8585f74d9d954611e975313aede0cce79a76236b
Ignacio Hagopian <jsign.uy@gmail.com>
2021-07-15 10:42:39 -0300

    msgbroker: add ackdeadline config & piecerd fixes
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ msgbroker/option.go
@@ -0,0 +1,31 @@
+package msgbroker
+
+import "time"
+
+var DefaultRegisterHandlerConfig = RegisterHandlerConfig{
+	AckDeadline: time.Second * 10,
+}
+
+type RegisterHandlerConfig struct {
+	AckDeadline time.Duration
+}
+
+type Option func(*RegisterHandlerConfig) error
+
+// WithACKDeadline configures the deadline for the message broker subscription.
+// The duration can't be greater than 10min.
+func WithACKDeadline(deadline time.Duration) Option {
+	return func(c *RegisterHandlerConfig) error {
+		c.AckDeadline = deadline
+		return nil
+	}
+}
+
+func ApplyRegisterHandlerOptions(opts ...Option) RegisterHandlerConfig {
+	config := DefaultRegisterHandlerConfig
+	for _, opt := range opts {
+		opt(&config)
+	}
+
+	return config
+}
