48260579f69b72b36b3a6813b2b8f949211ad154
Ignacio Hagopian <jsign.uy@gmail.com>
2021-07-27 11:15:29 -0300

    lints
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/dealerd/dealer/filclient/metrics.go
+++ cmd/dealerd/dealer/filclient/metrics.go
@@ -8,6 +8,7 @@ func (fc *FilClient) initMetrics() {
 	fc.metricExecAuctionDeal = metrics.Meter.NewInt64Counter("dealer.filclient.execauctiondeal")
 	fc.metricGetChainHeight = metrics.Meter.NewInt64Counter("dealer.filclient.getchainheight")
 	fc.metricResolveDealIDFromMessage = metrics.Meter.NewInt64Counter("dealer.filclient.resolvedealidfrommessage")
-	fc.metricCheckDealStatusWithStorageProvider = metrics.Meter.NewInt64Counter("dealer.filclient.checkdealstatuswithstorageprovider")
+	fc.metricCheckDealStatusWithStorageProvider =
+		metrics.Meter.NewInt64Counter("dealer.filclient.checkdealstatuswithstorageprovider")
 	fc.metricCheckChainDeal = metrics.Meter.NewInt64Counter("dealer.filclient.checkchaindeal")
 }
