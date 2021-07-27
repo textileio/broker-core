c4ecd89d2f7d1649e4ec2ed30a37b3f7badbad9c
Ignacio Hagopian <jsign.uy@gmail.com>
2021-07-27 09:17:33 -0300

    storaged: remove bearer tokens via flags
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/storaged/main.go
+++ cmd/storaged/main.go
@@ -28,7 +28,6 @@ func init() {
 		{Name: "ipfs-multiaddrs", DefValue: []string{}, Description: "IPFS multiaddresses"},
 		{Name: "pinata-jwt", DefValue: "",
 			Description: "Pinata API JWT to upload files also to Pinata. If empty, the feature will be considered disabled."},
-		{Name: "bearer-tokens", DefValue: []string{}, Description: "Raw accepted bearer tokens"},
 		{Name: "max-upload-size", DefValue: "4GB", Description: "Maximum upload size"},
 		{Name: "log-debug", DefValue: false, Description: "Enable debug level logging"},
 		{Name: "log-json", DefValue: false, Description: "Enable structured logging"},
@@ -47,7 +46,7 @@ var rootCmd = &cobra.Command{
 		common.CheckErrf("setting log levels: %v", err)
 	},
 	Run: func(c *cobra.Command, args []string) {
-		settings, err := common.MarshalConfig(v, !v.GetBool("log-json"), "bearer-tokens")
+		settings, err := common.MarshalConfig(v, !v.GetBool("log-json"))
 		common.CheckErr(err)
 		log.Infof("loaded config: %s", string(settings))
 
@@ -70,7 +69,6 @@ var rootCmd = &cobra.Command{
 			SkipAuth:              v.GetBool("skip-auth"),
 			IpfsMultiaddrs:        ipfsMultiaddrs,
 			PinataJWT:             v.GetString("pinata-jwt"),
-			BearerTokens:          common.ParseStringSlice(v, "bearer-tokens"),
 			MaxUploadSize:         v.GetSizeInBytes("max-upload-size"),
 		}
 		serv, err := service.New(serviceConfig)
