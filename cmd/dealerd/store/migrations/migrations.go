3c51d2dbfded627665f6adb7e608d2e1a72b099f
Ignacio Hagopian <jsign.uy@gmail.com>
2021-07-27 10:52:38 -0300

    rebase
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/dealerd/store/migrations/migrations.go
+++ cmd/dealerd/store/migrations/migrations.go
@@ -94,7 +94,7 @@ func _001_initDownSql() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "001_init.down.sql", size: 69, mode: os.FileMode(420), modTime: time.Unix(1627069278, 0)}
+	info := bindataFileInfo{name: "001_init.down.sql", size: 69, mode: os.FileMode(436), modTime: time.Unix(1626974831, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
@@ -114,11 +114,7 @@ func _001_initUpSql() (*asset, error) {
 		return nil, err
 	}
 
-<<<<<<< HEAD
-	info := bindataFileInfo{name: "001_init.up.sql", size: 1300, mode: os.FileMode(420), modTime: time.Unix(1627341929, 0)}
-=======
-	info := bindataFileInfo{name: "001_init.up.sql", size: 1333, mode: os.FileMode(436), modTime: time.Unix(1627392439, 0)}
->>>>>>> 654500d... rename: minerID to storageProviderID
+	info := bindataFileInfo{name: "001_init.up.sql", size: 1333, mode: os.FileMode(436), modTime: time.Unix(1627393919, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
