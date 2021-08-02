0e83ba1da4c8dbbf95ae731cb59d0a52d6aaf5c1
Sander Pick <sanderpick@gmail.com>
2021-08-02 13:51:58 -0700

    sqlc: regenerate
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- cmd/dealerd/store/migrations/migrations.go
+++ cmd/dealerd/store/migrations/migrations.go
@@ -94,7 +94,7 @@ func _001_initDownSql() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "001_init.down.sql", size: 69, mode: os.FileMode(420), modTime: time.Unix(1627069278, 0)}
+	info := bindataFileInfo{name: "001_init.down.sql", size: 69, mode: os.FileMode(420), modTime: time.Unix(1627934136, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
@@ -114,7 +114,7 @@ func _001_initUpSql() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "001_init.up.sql", size: 1333, mode: os.FileMode(420), modTime: time.Unix(1627396546, 0)}
+	info := bindataFileInfo{name: "001_init.up.sql", size: 1333, mode: os.FileMode(420), modTime: time.Unix(1627934136, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
