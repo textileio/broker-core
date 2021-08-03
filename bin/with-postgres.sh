131f0ff4c6d30ce8651c6c455b7cee505b081511
Merlin Ran <merlinran@gmail.com>
2021-08-03 13:20:29 -0400

    switch to one schema per daemon [shipit]
    Signed-off-by: Merlin Ran <merlinran@gmail.com>

--- bin/with-postgres.sh
+++ bin/with-postgres.sh
@@ -12,22 +12,27 @@ quit() {
 
 POSTGRES_USER="${POSTGRES_USER:-postgres}"
 DB_HOST="${DB_HOST:-127.0.0.1:5432}"
-DB_USER=${DAEMON}_user
-DB_NAME=${DAEMON}
+DB_NAME="${DB_NAME:-postgres}"
+DB_USER=${DAEMON}
 # just in case if errant newline sneaked into the k8s secret
 POSTGRES_PASSWORD=$(echo -n $POSTGRES_PASSWORD | tr -d \\n)
 DB_PASSWORD=$(echo -n $DB_PASSWORD | tr -d \\n)
 
 # when starting the pod, connect to postgres with admin privilege, create user
-# and database if not exist, and force change password, then starts daemon with
-# the correct postgres URI.
+# and schema if not exist, and force change password, then starts daemon with
+# the correct postgres URI. The use is restricted to the schema with the same name.
 psql "postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$DB_HOST" <<EOD
+SELECT 'CREATE DATABASE $DB_NAME WITH OWNER $POSTGRES_USER'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME')\gexec
+\connect $DB_NAME;
 SELECT 'CREATE USER $DB_USER'
 WHERE NOT EXISTS (SELECT FROM pg_roles WHERE rolname = '$DB_USER')\gexec
-GRANT $DB_USER TO $POSTGRES_USER;
-SELECT 'CREATE DATABASE $DB_NAME WITH OWNER $DB_USER'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$DB_NAME')\gexec
-ALTER USER $DB_USER WITH PASSWORD '$DB_PASSWORD'
+ALTER USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
+SELECT 'CREATE SCHEMA AUTHORIZATION $DB_USER'
+WHERE NOT EXISTS (SELECT FROM information_schema.schemata WHERE schema_name = '$DB_USER')\gexec
+GRANT ALL PRIVILEGES ON SCHEMA $DB_USER TO $DB_USER;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA $DB_USER TO $DB_USER;
+REVOKE CREATE ON SCHEMA public FROM PUBLIC;
 EOD
 [ $? -eq 0 ] || quit "fail to initialize postgres database"
 unset POSTGRES_PASSWORD
