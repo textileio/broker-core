9ee4e08392feec531db7b09180dea7c4070003aa
Ignacio Hagopian <jsign.uy@gmail.com>
2021-09-16 19:02:19 -0300

    relayd: avoid atomics and use updown counters directly
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/relayd/metrics/metrics.go
+++ cmd/relayd/metrics/metrics.go
@@ -2,13 +2,11 @@ package metrics
 
 import (
 	"context"
-	"sync/atomic"
 
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/multiformats/go-multiaddr"
 	logger "github.com/textileio/go-log/v2"
-	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/global"
 )
@@ -21,42 +19,33 @@ var (
 )
 
 type hostNotifee struct {
-	activeConns   int64
-	activeStreams int64
+	activeConns   metric.Int64UpDownCounter
+	activeStreams metric.Int64UpDownCounter
 }
 
 // Register exports libp2p connection metrics about the host.
 func Register(h host.Host) {
-	var mActiveConns metric.Int64ValueObserver
-	var mActiveStreams metric.Int64ValueObserver
-
-	var cn hostNotifee
-	batchObs := meter.NewBatchObserver(func(ctx context.Context, result metric.BatchObserverResult) {
-		result.Observe(
-			[]attribute.KeyValue{},
-			mActiveConns.Observation(atomic.LoadInt64(&cn.activeConns)),
-			mActiveStreams.Observation(atomic.LoadInt64(&cn.activeStreams)),
-		)
-	})
+	cn := hostNotifee{
+		activeConns:   meter.NewInt64UpDownCounter(prefix + ".active_conns"),
+		activeStreams: meter.NewInt64UpDownCounter(prefix + ".active_streams"),
+	}
 	h.Network().Notify(&cn)
-	mActiveConns = batchObs.NewInt64ValueObserver(prefix + ".active_conns")
-	mActiveStreams = batchObs.NewInt64ValueObserver(prefix + ".active_streams")
 }
 
 func (n *hostNotifee) Connected(_ network.Network, ne network.Conn) {
-	atomic.AddInt64(&n.activeConns, 1)
+	n.activeConns.Add(context.Background(), 1)
 	log.Debugf("%s connected", ne.RemotePeer())
 }
 func (n *hostNotifee) Disconnected(_ network.Network, ne network.Conn) {
-	atomic.AddInt64(&n.activeConns, -1)
+	n.activeConns.Add(context.Background(), -1)
 	log.Debugf("%s disconnected", ne.RemotePeer())
 }
 func (n *hostNotifee) OpenedStream(_ network.Network, s network.Stream) {
-	atomic.AddInt64(&n.activeStreams, 1)
+	n.activeStreams.Add(context.Background(), 1)
 	log.Debugf("%s opened stream %s (%s)", s.Conn().RemotePeer(), s.ID(), s.Protocol())
 }
 func (n *hostNotifee) ClosedStream(_ network.Network, s network.Stream) {
-	atomic.AddInt64(&n.activeStreams, -1)
+	n.activeStreams.Add(context.Background(), -1)
 	log.Debugf("%s closed stream %s (%s)", s.Conn().RemotePeer(), s.ID(), s.Protocol())
 }
 func (n *hostNotifee) ListenClose(_ network.Network, _ multiaddr.Multiaddr) {}
