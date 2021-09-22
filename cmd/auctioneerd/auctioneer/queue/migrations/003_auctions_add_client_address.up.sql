1529df7a592848d0cf1076e7decc27e504cda5f5
Merlin Ran <merlinran@gmail.com>
2021-09-22 11:22:46 -0700

    code review updates
    Signed-off-by: Merlin Ran <merlinran@gmail.com>

--- cmd/auctioneerd/auctioneer/queue/migrations/003_auctions_add_client_address.up.sql
+++ cmd/auctioneerd/auctioneer/queue/migrations/003_auctions_add_client_address.up.sql
@@ -1,2 +1,5 @@
-ALTER TABLE auctions ADD COLUMN client_address TEXT NOT NULL;
-
+BEGIN;
+ALTER TABLE auctions ADD COLUMN client_address TEXT;
+UPDATE auctions SET client_address = 'f144zep4gitj73rrujd3jw6iprljicx6vl4wbeavi';
+ALTER TABLE auctions ALTER COLUMN client_address SET NOT NULL;
+COMMIT;
