166cda403b0160309a1605881b3d9373675c40a7
Ignacio Hagopian <jsign.uy@gmail.com>
2021-10-21 17:51:45 -0300

    broker: fix migration [shipit]
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/brokerd/store/migrations/006_add_dealstartoffset.up.sql
+++ cmd/brokerd/store/migrations/006_add_dealstartoffset.up.sql
@@ -1,4 +1,5 @@
 BEGIN;
-ALTER TABLE batches ADD COLUMN IF NOT EXISTS proposal_start_offset_seconds BIGINT NOT NULL;
+ALTER TABLE batches ADD COLUMN IF NOT EXISTS proposal_start_offset_seconds BIGINT;
 UPDATE batches set proposal_start_offset_seconds=172800;
+ALTER TABLE batches ALTER COLUMN proposal_start_offset_seconds SET NOT NULL;
 COMMIT;
